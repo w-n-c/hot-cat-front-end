8b7638acd7d58d423b5b8c4d4c7ff13c8396dcae
Wil.ly Hellcat <newellwm@gmail.com>
2017-08-11 19:15:06 -0700

    Add functional file opening and closing

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 (function() {
-	let   filepath  = "";
 	const editor    = ace.edit("editor");
+	const filepath  = "api/v0/file";
 	const files     = document.getElementsByTagName("d2l-menu-item");
 	const dirs      = document.getElementsByTagName("d2l-menu");
 	const menuBtn   = document.getElementById("menu-button");
 	const menuRoot  = document.getElementById("menu");
+	let currentFile = filepath;
 
 	const getData = (url) => {
 		return new Promise((res, rej) => {
@@ -13,6 +14,16 @@
 			xhr.onload = () => res(JSON.parse(xhr.responseText))
 			xhr.onerror = () => rej(xhr.statusText);
 			xhr.send();
+		});
+	};
+	const sendData = (url, data) => {
+		return new Promise((res, rej) => {
+			const xhr = new XMLHttpRequest();
+			xhr.open("POST", url, true);
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhr.onload = () => res(true);
+			xhr.onerror = () => rej(xhr.statusText);
+			xhr.send(data);
 		});
 	};
 	const appDirMenu = (obj, root) => {
@@ -38,29 +49,39 @@
 		});
 	};
 	const toggleDisplay = (el) => {
-		// causes a glitch with media query, need to update
+		// causes a bug with media query, need to update
 		(style => {
 			style.display = style.display === 'none' ? '' : 'none';
 		})(el.style);
 	};
-	const managePath = (el) => {
-		//used to build and track a directory string
-		//on click check if name at end of string, if so remove else add`
+	const changeFile = (event) => {
+		if (currentFile != filepath) {
+			sendData(currentFile, editor.getValue())
+			.catch(reason => {
+				console.log("Failed to post file changes: " + reason);
+			});
+		}
+		currentFile = filepath;
+		//event path starts at the local element and ends at html
+		for(let i = event.path.length - 1; i >= 0; i--) {
+			const el = event.path[i]
+			if (el.nodeName === 'D2L-MENU') {
+				if (el.getAttribute("label") === "filesystem");
+				else {
+					currentFile += `/${el.getAttribute("label")}`;
+				}
+			}
+		};
+		currentFile += `/${event.target.getAttribute("text")}`;
+		getData(currentFile).then(file => {
+			editor.setValue(file);
+		}).catch(reason => {
+			console.log("File loading failed: " + reason);
+		});
 	}
-	const changeFile = (el) => {
-		//editor.getValue();
-		//post value
-		//request new file contents
-		//editor.setValue(contents)
-	}
-	
 	menuBtn.onclick = () => { toggleDisplay(menu) };
-	Array.from(dirs).forEach( el => {
-		el.addEventListener('click', () => { managePath(el) });
-	});
-	Array.from(files).forEach( el => {
-		el.addEventListener('click', () => { changeFile(el) });
-	});
+	menu.addEventListener('d2l-menu-item-select', (e) => { changeFile(e) });
+
 	getData("/api/v0/directory").then(directory => {
 		appDirMenu(directory, menuRoot);
 	}).catch(reason => {
@@ -68,6 +89,7 @@
 	});
 	editor.setTheme("ace/theme/monokai");
 	editor.getSession().setMode("ace/mode/javascript");
+	editor.$blockScrolling = Infinity;
 	document.getElementById('editor').style.fontSize='14px';
 })();
 
